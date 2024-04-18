1b62cb48015f67409f7a6b9c584eee664bed011d
TeamWebLux <129931568+surajy1456@users.noreply.github.com>
2024-04-18 18:53:22 +0530

    created index page
    created index page with reg form by giving label and placeholder

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reg Form</title>
+</head>
+<body>
+    <div class="regform">
+        <form action="" class="form">
+            <div class="form-control">
+                <label for="">Name:</label>
+                <input type="text" name="name" id="" placeholder="Name" required>
+            </div>
+            <div class="form-control">
+                <label for="">Email:</label>
+                <input type="email" name="email" id="" placeholder="Email" required>
+            </div>
+            <div class="form-control">
+                <label for="">Phone No:</label>
+                <input type="text" name="number" id="" placeholder="Number" required>
+            </div>
+            <div class="form-control">
+                <label for="">Subject:</label>
+                <input type="text" name="subject" id="" placeholder="Subject" required>
+            </div>
+            <div class="form-control-message">
+                <label for="">Message:</label>
+                <input type="text" name="message" id="" placeholder="Message" required>
+            </div>
+            <div class="btn">
+            <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>    
+</body>
+</html>
