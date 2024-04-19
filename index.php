d0bef70268f5899311fa5001ad10d2042714297b
surajy1456 <surajy1456@gmail.com>
2024-04-19 20:45:30 +0530

    upddaatee

--- index.php
+++ index.php
@@ -5,42 +5,101 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reg Form</title>
     <style>
-     
-        .regform{
-            width: 500px;
-            height: 600px;
+        
+        img{
+            width: 1300px; 
+            height: 800px;  
+        }
+         
+
+       
+
+        .form {
+           
+        }
+
+        .form-control {
+            margin-bottom: 10px;
+        }
+
+        .form-control input {
+         
+        }
+
+        .btn {
             text-align: center;
-            border: 10px solid black;
+        }
+
+        .btn button {
+            padding: 10px 20px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        #for_name{
+            height:60px;
+            width:460px;
+            position:absolute;
+            top:266px;
+            left:162px;
+            border-radius:40px;
+            border:transparent;
+            background:transparent;
+            color:white;
+            padding-left: 23px;
+            font-size:30px;
+        }
+        #for_number{
+            height:60px;
+            width:460px;
+            position:absolute;
+            top:406px;
+            left:162px;
+            border-radius:40px;
+            border:transparent;
+            background:transparent;
+            color:white;
+            padding-left: 23px;
+            font-size:30px;
+        }
+        #for_email{
+            height:60px;
+            width:440px;
+            position:absolute;
+            top:546px;
+            left:162px;
+            border-radius:40px;
+            border:transparent;
+            background:transparent;
+            color:white;
+            padding-left: 23px;
+            font-size:30px;
         }
     </style>
 </head>
 <body>
-    <div class="regform">
-        <form action="" class="form">
-            <div class="form-control">
-                <label for="">Name:</label>
-                <input type="text" name="name" id="" placeholder="Name" required>
-            </div>
-            <div class="form-control">
-                <label for="">Email:</label>
-                <input type="email" name="email" id="" placeholder="Email" required>
-            </div>
-            <div class="form-control">
-                <label for="">Phone No:</label>
-                <input type="text" name="number" id="" placeholder="Number" required>
-            </div>
-            <div class="form-control">
-                <label for="">Subject:</label>
-                <input type="text" name="subject" id="" placeholder="Subject" required>
-            </div>
-            <div class="form-control-message">
-                <label for="">Message:</label>
-                <input type="text" name="message" id="" placeholder="Message" required>
-            </div>
-            <div class="btn">
-            <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-    </div>    
-</body>
-</html>
+    <center>
+        
+        <div class="regform">
+            <img src="vegas registration.png" alt=""  >
+            <form action="" class="form">
+                <div class="form-control">
+                    <input type="text" name="name" id="for_name" placeholder="Name" required>
+                </div>
+                <div class="form-control">
+                    <input type="text" name="number" id="for_number" placeholder="Number" required>
+                </div>
+                <div class="form-control">
+                    <input type="email" name="email" id="for_email" placeholder="Email" required>
+                </div>
+                
+                <div class="btn">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>    
+    </center>
+    </body>
+    </html>
