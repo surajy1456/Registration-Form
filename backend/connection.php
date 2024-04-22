6c6af913e898b126bcf1d1293c24572a162eb099
surajy1456 <129931568+surajy1456@users.noreply.github.com>
2024-04-22 14:09:57 +0530

    updated database name

--- backend/connection.php
+++ backend/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = new mysqli("localhost","root","","reg_data");
+$conn = new mysqli("62.72.28.92","u114668675_suraj","Suraj@1456","u114668675_reg_form");
 
 if ($conn-> connect_errno) {
     echo "databaseb is not connected:".$mysqli->connect_error;
