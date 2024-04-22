d411d5b8f0520b8ed149e62e77cd094a515881e4
surajy1456 <129931568+surajy1456@users.noreply.github.com>
2024-04-22 14:11:08 +0530

    updated localhost name

--- backend/connection.php
+++ backend/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = new mysqli("62.72.28.92","u114668675_suraj","Suraj@1456","u114668675_reg_form");
+$conn = new mysqli("heroitec.cloud","u114668675_suraj","Suraj@1456","u114668675_reg_form");
 
 if ($conn-> connect_errno) {
     echo "databaseb is not connected:".$mysqli->connect_error;
