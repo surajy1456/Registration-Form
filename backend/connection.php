566efd20ba1b07ca2965b9d26f89dd4398d539c4
surajy1456 <129931568+surajy1456@users.noreply.github.com>
2024-04-22 14:15:21 +0530

    updated connection

--- backend/connection.php
+++ backend/connection.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$conn = new mysqli("heroitec.cloud","u114668675_suraj","Suraj@8879","u114668675_reg_form");
+$conn = mysqli_connect("heroitec.cloud","u114668675_suraj","Suraj@8879","u114668675_reg_form");
 
-if ($conn-> connect_errno) {
+if (mysqli_connect_errno()) {
     echo "databaseb is not connected:".$mysqli->connect_error;
     exit();
 }
